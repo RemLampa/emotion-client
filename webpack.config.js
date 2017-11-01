aaf58bc7d43edab008c8e9a72a1f5097ae035eca
ibleedfilm <rem@lansangan.com>
2017-11-01 19:50:37 +0800

    :heavy_plus_sign: Install react

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['es2015']
+                            presets: ['es2015', 'env', 'react']
                         }
                     }
                 ]
