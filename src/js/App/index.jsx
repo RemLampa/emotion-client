b1e895f44b5ca3ce3722d37e469b4a6ba824e4aa
ibleedfilm <rem@lansangan.com>
2017-11-05 12:04:54 +0800

    :sparkles: Add button to wake up Heroku-hosted API

--- src/js/App/index.jsx
+++ src/js/App/index.jsx
@@ -1,3 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export default () => (<h1>Hello World!</h1>);
+export default class App extends Component {
+    constructor () {
+        super();
+
+        this.state = {
+            isApiAwake: false,
+            isPingingApi: false
+        };
+
+        this.apiUrl = 'https://spooky-plague-26435.herokuapp.com/';
+
+        this.renderButton = this.renderButton.bind(this);
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+    }
+
+    renderUI () {
+        const { isApiAwake } = this.state;
+
+        return isApiAwake
+            ? ''
+            : this.renderButton();
+    }
+
+    renderButton () {
+        const isDisabled = this.state.isPingingApi;
+
+        return (
+            <button onClick={this.handleButtonClick} disabled={isDisabled}>
+                { isDisabled ? 'Contacting Server...' : 'Activate Emotion Server' }
+            </button>
+        );
+    }
+
+    handleButtonClick (e) {
+        e.preventDefault();
+
+        console.log('yes');
+        this.setState({isPingingApi: true});
+
+        return axios.get(this.apiUrl);
+    }
+
+    render () {
+        return (
+            <div>
+                { this.renderUI() }
+            </div>
+        );
+    }
+};
--- test/App/index.spec.jsx
+++ test/App/index.spec.jsx
@@ -1,14 +1,76 @@
+import axios from 'axios';
+
 import App from 'App';
 
 describe('App', function () {
-    let component;
+    let wrapper;
 
     beforeEach(function () {
-        component = shallow(<App />);
+        wrapper = shallow(<App />);
     });
 
-    it('should render "Hello World"', function () {
-        expect(component).to.have.type('h1');
-        expect(component).to.have.text('Hello World!');
+    it('should be a div', function () {
+        expect(wrapper).to.have.type('div');
+    });
+
+    context('Initial State', function () {
+        it('should have proper initial state', function () {
+            expect(wrapper).to.have.state('isApiAwake').equal(false);
+            expect(wrapper).to.have.state('isPingingApi').equal(false);
+        });
+
+        it('should render <button />', function () {
+            const buttonWrapper = wrapper.find('button').first();
+
+            expect(wrapper).to.have.exactly(1).descendants('button');
+
+            expect(buttonWrapper).to.have.prop('disabled').equal(false);
+            expect(buttonWrapper).to.have.prop('onClick', wrapper.instance().handleButtonClick);
+            expect(buttonWrapper).to.have.text('Activate Emotion Server');
+        });
+    });
+
+    context('::button', function () {
+        let buttonWrapper;
+        let requestStub;
+        let apiUrl;
+        let successRes;
+        let event;
+        let clickSpy;
+
+        beforeEach(function () {
+            clickSpy = spy(App.prototype, 'handleButtonClick');
+            wrapper = shallow(<App />);
+
+            buttonWrapper = wrapper.find('button');
+            requestStub = stub(axios, 'get');
+            apiUrl = 'https://spooky-plague-26435.herokuapp.com/';
+            successRes = JSON.stringify({index: "Hello World!"});
+            event = { preventDefault: () => null };
+        });
+
+        afterEach(function () {
+            requestStub.restore();
+            clickSpy.restore();
+        });
+
+        it('should call handleButtonClick method when clicked', function () {
+            buttonWrapper.simulate('click', event);
+
+            expect(clickSpy).to.have.been.calledOnce;
+
+            expect(wrapper).to.have.state('isPingingApi').equal(true);
+            expect(wrapper.find('button').first()).text('Contacting Server...');
+
+            expect(requestStub).to.have.been.calledWith(apiUrl);
+        });
+
+        it('should make HTTP request to API when clicked', function () {
+            requestStub.withArgs(apiUrl).returns(successRes);
+
+            buttonWrapper.simulate('click', event);
+
+            // expect(wrapper).to.
+        });
     });
 });
